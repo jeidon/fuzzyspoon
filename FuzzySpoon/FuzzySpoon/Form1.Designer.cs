5a046aa704b92eeef22072c13b3b835841bbe30d
Max Roberg <max@therobergs.com>
2016-09-02 23:24:04 -0700

    Added first layer of UI components and initial LINQ database stuff

---
+++ FuzzySpoon/FuzzySpoon/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FuzzySpoon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbController = new System.Windows.Forms.ComboBox();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbBaud = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmdConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Controller";
+            // 
+            // cmbController
+            // 
+            this.cmbController.FormattingEnabled = true;
+            this.cmbController.Location = new System.Drawing.Point(12, 25);
+            this.cmbController.Name = "cmbController";
+            this.cmbController.Size = new System.Drawing.Size(121, 21);
+            this.cmbController.TabIndex = 1;
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(12, 65);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(121, 21);
+            this.cmbPort.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port";
+            // 
+            // cmbBaud
+            // 
+            this.cmbBaud.FormattingEnabled = true;
+            this.cmbBaud.Location = new System.Drawing.Point(12, 105);
+            this.cmbBaud.Name = "cmbBaud";
+            this.cmbBaud.Size = new System.Drawing.Size(121, 21);
+            this.cmbBaud.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Baud";
+            // 
+            // cmdConnect
+            // 
+            this.cmdConnect.Location = new System.Drawing.Point(58, 132);
+            this.cmdConnect.Name = "cmdConnect";
+            this.cmdConnect.Size = new System.Drawing.Size(75, 23);
+            this.cmdConnect.TabIndex = 6;
+            this.cmdConnect.Text = "button1";
+            this.cmdConnect.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 261);
+            this.Controls.Add(this.cmdConnect);
+            this.Controls.Add(this.cmbBaud);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbController);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Windows Based OLED Controll Panel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbController;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbBaud;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button cmdConnect;
+    }
+}
+
---
+++ FuzzySpoon/FuzzySpoon/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuzzySpoon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Fill the controllers dropdown
+            try
+            {
+                cmbController.Items.Clear();
+                cmbController.Text = "Select a controller";
+
+                Controller controller = new Controller();
+                if (controller.Commands != null)
+                {
+                    var controllers = from d in controller.ControllerName
+                                      select d;
+                    foreach (var item in controllers)
+                    {
+                        cmbController.Items.Add(item);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("An error has occured while loading the controller list");
+            }
+
+            //Fill the port dropdown
+
+            //Fill the baud rate dropdown
+            try
+            {
+                cmbBaud.Items.Clear();
+                cmbBaud.Text = "Select a Baud Rate";
+
+                BaudRate baudRate = new BaudRate();
+                if (baudRate.Baud != null)
+                {
+
+                    var BaudRates = from d in baudRate.Baud
+                                    select d;
+                    foreach (var item in BaudRates)
+                    {
+                        cmbController.Items.Add(item);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("An error has occured while loading the controller list");
+            }
+
+        }
+    }
+
+    public class Port
+    {
+        //These have no get or set because they will populate on the fly
+        public int PortID;
+        public string PortName;
+    }
+
+    public class BaudRate
+    {
+        public int BaudID { get; set; }
+        public string Baud { get; set; }
+    }
+
+    public class Controller
+    {
+        public int ControllerID { get; set; }
+        public string ControllerName { get; set; }
+
+        public ICollection<Command> Commands { get; set; }
+    }
+
+    public class Command
+    {
+        public int CommandID { get; set; }
+        public string CommandName { get; set; }
+        public string CommandValue { get; set; }
+        public ICollection<int> Parameters { get; set; }
+
+        public virtual Controller Controller { get; set; }
+    }
+
+    public class ControllerCommands : DbContext
+    {
+        public DbSet<BaudRate> BaudRates { get; set; }
+        public DbSet<Controller> Controllers { get; set; }
+        public DbSet<Command> Commands { get; set; }
+    }
+}
